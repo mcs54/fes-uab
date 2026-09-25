f858da9242f29ab53c1c2159487c904f95340010
mcs54 <marticarrerasserra@gmail.com>
2026-09-25 10:29:16 +0200

    exercici 1

---
+++ 01_print.py
@@ -0,0 +1,17 @@
+###
+# Exercicis - print()
+# Completa els exercicis següents utilitzant la funció print()
+###
+
+# Exercici 1
+# Imprimeix el teu nom i cognom a la consola
+print("Martí Carreras Serra")
+
+# Exercici 2
+# Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
+# Pista: utilitza el paràmetre 'sep' de print()
+print("Python", "és", "brutal", sep = "-")
+
+# Exercici 3
+# Imprimeix el resultat de la suma 15 + 27
+print(15+27)
